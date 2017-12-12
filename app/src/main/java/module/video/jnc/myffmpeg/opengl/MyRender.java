ffdd456245b1d7a8c208585d2d28af1b92eaf0ab
xhcjnc <1051261635@qq.com>
2017-12-12 10:50:56 +0800

    1，正交投影

--- app/src/main/java/module/video/jnc/myffmpeg/opengl/MyRender.java
+++ app/src/main/java/module/video/jnc/myffmpeg/opengl/MyRender.java
@@ -12,7 +12,8 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import module.video.jnc.myffmpeg.R;
-
+import android.opengl.GLES20;
+import android.opengl.Matrix;
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_LINES;
@@ -27,6 +28,7 @@ import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
@@ -49,6 +51,12 @@ public class MyRender implements GLSurfaceView.Renderer{
     private static final String A_POSITION = "a_Position";
     private int aPositionLocation ;
 
+    private static final String U_MATRIX = "u_Matrix";
+    private int uMatrixLocation ;
+
+    private final float[] projectionMatrix = new float[16];
+
+
     private static final int BYTES_PER_FLOAT = 4;
     private final FloatBuffer vertexData;
     float[] tableVerticesWithTriangles = {
@@ -67,6 +75,7 @@ public class MyRender implements GLSurfaceView.Renderer{
     private static final int COLOR_COMPONENT_COUNT = 3;
     //数组中因为不全是顶点的坐标，还有颜色等，要告诉opengl中间有多少颜色等。
     private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
     public MyRender(Context context){
         this.context = context;
         vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT )
@@ -104,7 +113,7 @@ public class MyRender implements GLSurfaceView.Renderer{
         //找到颜色属性的位置，绘制颜色的时候要使用
 //        uColorLocation = glGetUniformLocation(program , U_COLOR);
         aColorLocation = glGetAttribLocation(program , A_COLOR);
-
+        uMatrixLocation = glGetUniformLocation(program , U_MATRIX);
         //找到位置属性的位置
         aPositionLocation = glGetAttribLocation(program , A_POSITION);
         Log.e("xhc" , " aPositionLocation "+aPositionLocation);
@@ -125,13 +134,38 @@ public class MyRender implements GLSurfaceView.Renderer{
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
-        glViewport(0 , 0 , i , i1);
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        glViewport(0 , 0 , width , height);
+        final float aspectRation = width > height ? (float) width / (float) height : (float)height / (float)width;
+
+        /**
+         * float[] m, int mOffset, float left, float right, float bottom, float top, float near, float far
+         * m:目标数组
+         * mOffset：结果矩阵的其实偏移值
+         * left: x轴最小的范围
+         * right：x轴最大的范围
+         * bottom：y轴最小的范围
+         * top：y轴最大的范围
+         * near：z轴最小的范围
+         * far：z轴最大的范围
+         * 这个函数就是生成一个正交矩阵，就是将以前在屏幕上的坐标范围（-1，1）改变下e.g（-1.78,1.78）。
+         * 如果按上述方式改变下就是相对就更“聚拢”了。
+         */
+        Log.e("xhc" , "aspectRation "+aspectRation);
+        if(width > height){
+            Matrix.orthoM(projectionMatrix , 0 ,  -aspectRation , aspectRation ,-1f, 1f , -1f , 1f);
+//            Matrix.orthoM(projectionMatrix , 0 ,  -3f , 3f ,-1f, 1f , -1f , 1f);
+        }
+        else{
+            Matrix.orthoM(projectionMatrix , 0 , -1f, 1f , -aspectRation , aspectRation , -1f , 1f);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
+        //正交投影
+        glUniformMatrix4fv(uMatrixLocation , 1 , false , projectionMatrix , 0);
 
 //        glUniform4f(uColorLocation ,1.0f ,1.0f ,1.0f,1.0f);
         glDrawArrays(GL_TRIANGLE_FAN , 0 , 6);
--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -2,8 +2,9 @@
 attribute vec4 a_Position ;
 attribute vec4 a_Color ;
 varying vec4 v_Color;
+uniform mat4 u_Matrix;
 void main(){
     v_Color = a_Color;
-    gl_Position = a_Position;
+    gl_Position = a_Position * u_Matrix;
     gl_PointSize = 10.0;
 }
