0edbc62038a19f27d6e011af37803c6d2bcd8472
admin <273045923@qq.com>
2018-01-08 22:07:44 +0800

    1.一些简单的程序解读

--- app/src/main/java/module/video/jnc/myffmpeg/MyRecordActivity.java
+++ app/src/main/java/module/video/jnc/myffmpeg/MyRecordActivity.java
@@ -11,26 +11,28 @@ import module.video.jnc.myffmpeg.EGLCamera.CameraManeger;
 
 public class MyRecordActivity extends AppCompatActivity {
 
-    private SurfaceView surfaceView;
     private FrameLayout fl ;
     CameraManeger cm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_record);
+        FFmpegUtils.myInit(Constant.rootFile.getAbsolutePath()+"/my_camera.MP4" , CameraManeger.width  , CameraManeger.height);
         fl = findViewById(R.id.container);
         cm = new CameraManeger();
         Camera camera = cm.OpenCamera();
         CameraPreview cp = new CameraPreview(this , camera);
         fl.addView(cp);
-        camera.setPreviewCallback(new Camera.PreviewCallback(){
+        camera.setPreviewCallbackWithBuffer(new Camera.PreviewCallback(){
             @Override
             public void onPreviewFrame(byte[] bytes, Camera camera) {
+
+                Log.e("xhc" , " onPreviewFrame ");
                 FFmpegUtils.encodeCamera(bytes);
             }
         });
 
-        FFmpegUtils.myInit(Constant.rootFile.getAbsolutePath()+"/my_camera.MP4" , CameraManeger.width  , CameraManeger.height);
+
 
     }
 
