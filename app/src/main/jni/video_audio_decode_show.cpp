bae392ea20d1729ed332f3e65a46a9a0abbe4d4c
admin <273045923@qq.com>
2018-04-26 07:51:12 +0800

    音视频都能同时播放了。 1.接下来还需要解决缓冲区太大的问题（需要线程的阻塞等来处理） 2.同步播放的问题（控制时间）

--- app/src/main/jni/video_audio_decode_show.cpp
+++ app/src/main/jni/video_audio_decode_show.cpp
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include "video_audio_decode_show.h"
 #include <queue>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+
 extern "C" {
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
@@ -19,13 +22,13 @@ const static int RESULT_FAILD = -1;
 queue<char *> audioQue;
 SLObjectItf engineOpenSL = NULL;
 SLPlayItf iplayer_ = NULL;
-SLEngineItf eng_ =  NULL;
-SLObjectItf mix_ =  NULL;
-SLObjectItf  player_ = NULL;
+SLEngineItf eng_ = NULL;
+SLObjectItf mix_ = NULL;
+SLObjectItf player_ = NULL;
 
 //FILE *filePcm ;
 char *buf_ = NULL;
-SLAndroidSimpleBufferQueueItf  pcmQue_ = NULL;
+SLAndroidSimpleBufferQueueItf pcmQue_ = NULL;
 bool playFlag = false;
 
 
@@ -50,18 +53,17 @@ SLEngineItf createOpenSL() {
     return en;
 }
 
-void pcmCallBack(SLAndroidSimpleBufferQueueItf bf , void *context){
+void pcmCallBack(SLAndroidSimpleBufferQueueItf bf, void *context) {
 
 
-    if(!buf_)
-    {
-        buf_ = new char[1024*1024];
+    if (!buf_) {
+        buf_ = new char[1024 * 1024];
     }
-    LOGE(" ADD BUFFER audioQue.size %d ",audioQue.size() );
-    if(!audioQue.empty()){
+    LOGE(" ADD BUFFER audioQue.size %d ", audioQue.size());
+    if (!audioQue.empty()) {
         char *pointer = audioQue.front();
         audioQue.pop();
-        (*bf)->Enqueue(bf,pointer,2048);
+        (*bf)->Enqueue(bf, pointer, 2048);
     }
 //    if(!filePcm)
 //    {
@@ -85,28 +87,28 @@ int play_audio_stream() {
 //    pcm_path = path;
     //创建引擎
     eng_ = createOpenSL();
-    if(!eng_){
+    if (!eng_) {
         LOGE("createSL FAILD ");
     }
 
     //2.创建混音器
     mix_ = NULL;
     SLresult re = 0;
-    re = (*eng_)->CreateOutputMix(eng_ , &mix_ , 0 ,0 , 0);
-    if(re != SL_RESULT_SUCCESS){
+    re = (*eng_)->CreateOutputMix(eng_, &mix_, 0, 0, 0);
+    if (re != SL_RESULT_SUCCESS) {
         LOGE("CreateOutputMix FAILD ");
         return RESULT_FAILD;
     }
     re = (*mix_)->Realize(mix_, SL_BOOLEAN_FALSE);
-    if(re != SL_RESULT_SUCCESS){
+    if (re != SL_RESULT_SUCCESS) {
         LOGE("Realize FAILD ");
         return RESULT_FAILD;
     }
-    SLDataLocator_OutputMix outmix = {SL_DATALOCATOR_OUTPUTMIX , mix_};
-    SLDataSink audioSink = {&outmix , 0};
+    SLDataLocator_OutputMix outmix = {SL_DATALOCATOR_OUTPUTMIX, mix_};
+    SLDataSink audioSink = {&outmix, 0};
 
     //配置音频信息
-    SLDataLocator_AndroidSimpleBufferQueue que = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE , 10};
+    SLDataLocator_AndroidSimpleBufferQueue que = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 10};
     //音频格式
     SLDataFormat_PCM pcm_ = {
             SL_DATAFORMAT_PCM,
@@ -117,29 +119,30 @@ int play_audio_stream() {
             SL_SPEAKER_FRONT_LEFT,
             SL_BYTEORDER_LITTLEENDIAN //字节序，小端
     };
-    SLDataSource ds = {&que,&pcm_};
+    SLDataSource ds = {&que, &pcm_};
 
     //创建播放器
     const SLInterfaceID ids[] = {SL_IID_BUFFERQUEUE};
     const SLboolean req[] = {SL_BOOLEAN_TRUE};
-    re = (*eng_)->CreateAudioPlayer(eng_ , &player_ , &ds , &audioSink , sizeof(ids) / sizeof(SLInterfaceID) , ids , req);
-    if(re != SL_RESULT_SUCCESS){
+    re = (*eng_)->CreateAudioPlayer(eng_, &player_, &ds, &audioSink,
+                                    sizeof(ids) / sizeof(SLInterfaceID), ids, req);
+    if (re != SL_RESULT_SUCCESS) {
         LOGE("CreateAudioPlayer FAILD ");
         return RESULT_FAILD;
     }
-    (*player_)->Realize(player_,SL_BOOLEAN_FALSE);
-    re = (*player_)->GetInterface(player_ , SL_IID_PLAY , &iplayer_);
-    if(re != SL_RESULT_SUCCESS){
+    (*player_)->Realize(player_, SL_BOOLEAN_FALSE);
+    re = (*player_)->GetInterface(player_, SL_IID_PLAY, &iplayer_);
+    if (re != SL_RESULT_SUCCESS) {
         LOGE("GetInterface SL_IID_PLAY FAILD ");
         return RESULT_FAILD;
     }
-    re = (*player_)->GetInterface(player_ , SL_IID_BUFFERQUEUE , &pcmQue_);
-    if(re != SL_RESULT_SUCCESS){
+    re = (*player_)->GetInterface(player_, SL_IID_BUFFERQUEUE, &pcmQue_);
+    if (re != SL_RESULT_SUCCESS) {
         LOGE("GetInterface SL_IID_BUFFERQUEUE FAILD ");
         return -1;
     }
 
-    (*pcmQue_)->RegisterCallback(pcmQue_ , pcmCallBack , 0);
+    (*pcmQue_)->RegisterCallback(pcmQue_, pcmCallBack, 0);
 
 //    (*iplayer_)->SetPlayState(iplayer_ , SL_PLAYSTATE_PLAYING);
 //
@@ -149,14 +152,15 @@ int play_audio_stream() {
 }
 
 //播放或者暂停音频
-int playOrPauseAudio(){
-    if(iplayer_ != NULL){
+int playOrPauseAudio() {
+    if (iplayer_ != NULL) {
 
-        SLresult re = (*iplayer_)->SetPlayState(iplayer_ , playFlag == true ? SL_PLAYSTATE_PLAYING : SL_PLAYSTATE_PAUSED);
-        if(playFlag){
-            (*pcmQue_)->Enqueue(pcmQue_ , "" , 1);
+        SLresult re = (*iplayer_)->SetPlayState(iplayer_, playFlag == true ? SL_PLAYSTATE_PLAYING
+                                                                           : SL_PLAYSTATE_PAUSED);
+        if (playFlag) {
+            (*pcmQue_)->Enqueue(pcmQue_, "", 1);
         }
-        if(re != SL_RESULT_SUCCESS){
+        if (re != SL_RESULT_SUCCESS) {
             LOGE("SetPlayState pause FAILD ");
             return RESULT_FAILD;
         }
@@ -165,18 +169,18 @@ int playOrPauseAudio(){
     return RESULT_SUCCESS;
 }
 
-int audioDestroy(){
-    if(player_ != NULL){
+int audioDestroy() {
+    if (player_ != NULL) {
         (*player_)->Destroy(player_);
         player_ = NULL;
         iplayer_ = NULL;
         pcmQue_ = NULL;
     }
-    if(mix_ != NULL){
+    if (mix_ != NULL) {
         (*mix_)->Destroy(mix_);
         mix_ = NULL;
     }
-    if(engineOpenSL != NULL){
+    if (engineOpenSL != NULL) {
         (*engineOpenSL)->Destroy(engineOpenSL);
         engineOpenSL = NULL;
         eng_ = NULL;
@@ -204,13 +208,27 @@ AVFrame *frame_ = NULL;
 SwsContext *sws_ = NULL;
 char *pcm_ = NULL;
 SwrContext *actx_ = NULL;
+char *rgb_ = NULL;
+int outWidth_ = 0;
+int outHeight_ = 0;
+ANativeWindow_Buffer wbuf_;
+ANativeWindow *aWindow;
 
-int initFFmpeg(const char* input_path){
+int initWindow(JNIEnv *env, jobject surface) {
+    aWindow = ANativeWindow_fromSurface(env, surface);
+    ANativeWindow_setBuffersGeometry(aWindow, outWidth_, outHeight_, WINDOW_FORMAT_RGBA_8888);
+    return RESULT_SUCCESS;
+}
+
+int initFFmpeg(const char *input_path) {
     int result = 0;
     av_register_all();
     avcodec_register_all();
+
+
     pkt_ = av_packet_alloc();
     frame_ = av_frame_alloc();
+
 
     result = avformat_open_input(&afc_, input_path, 0, 0);
     if (result != 0) {
@@ -232,7 +250,8 @@ int initFFmpeg(const char* input_path){
             video_index_ = i;
 
             LOGE("VIDEO WIDTH %d , HEIGHT %d , format %d , fps %f ", avStream->codecpar->width,
-                 avStream->codecpar->height, avStream->codecpar->format ,av_q2d(avStream->avg_frame_rate ));
+                 avStream->codecpar->height, avStream->codecpar->format,
+                 av_q2d(avStream->avg_frame_rate));
 
             videoCode_ = avcodec_find_decoder(avStream->codecpar->codec_id);
             if (!videoCode_) {
@@ -286,21 +305,21 @@ int initFFmpeg(const char* input_path){
         return RESULT_FAILD;
     }
 
-    int outWidth = vc_->width;
-    int outHeight = vc_->height;
+    outWidth_ = vc_->width;
+    outHeight_ = vc_->height;
     int frameCount = 0;
 
-    char *rgb = new char[outWidth * outHeight * 4];
-     pcm_ = new char[48000 * 4 * 2];
+    rgb_ = new char[outWidth_ * outHeight_ * 4];
+    pcm_ = new char[48000 * 4 * 2];
 
     //音频重采样上下文初始化
     actx_ = swr_alloc();
     actx_ = swr_alloc_set_opts(actx_,
-                              av_get_default_channel_layout(1),
-                              AV_SAMPLE_FMT_S16, ac_->sample_rate,
-                              av_get_default_channel_layout(ac_->channels),
-                              ac_->sample_fmt, ac_->sample_rate,
-                              0, 0);
+                               av_get_default_channel_layout(1),
+                               AV_SAMPLE_FMT_S16, ac_->sample_rate,
+                               av_get_default_channel_layout(ac_->channels),
+                               ac_->sample_fmt, ac_->sample_rate,
+                               0, 0);
     result = swr_init(actx_);
     if (result < 0) {
         LOGE(" swr_init FAILD !");
@@ -311,7 +330,7 @@ int initFFmpeg(const char* input_path){
 }
 
 //测试函数
-void testPlay(){
+void testPlay() {
     playFlag = true;
     playOrPauseAudio();
 }
@@ -320,18 +339,23 @@ int videoAudioOpen(JNIEnv *env, jobject surface, const char *path) {
     LOGE("videoAudioOpen %s  ", path);
     int result = -1;
     result = initOpenSlEs();
-    if(result == RESULT_FAILD){
+    if (result == RESULT_FAILD) {
         LOGE(" initOpenSlEs FAILD ! ");
         return RESULT_FAILD;
     }
     result = initFFmpeg(path);
-    if( result == RESULT_FAILD ){
+    if (result == RESULT_FAILD) {
         LOGE(" initFFmpeg FAILD ! ");
         return RESULT_FAILD;
     }
-    playFlag = true;
-    playOrPauseAudio();
+    result = initWindow(env, surface);
+    if (result == RESULT_FAILD) {
+        LOGE(" initWindow FAILD ! ");
+        return RESULT_FAILD;
+    }
 
+    //控制音频的播放，音频里面有缓冲数据的时候，并且播放状态是playing的时候才能开始播放，后面正式的时候用解码线程，和播放线程应该能解决这个问题
+    int audioCount = 0;
     while (true) {
         result = av_read_frame(afc_, pkt_);
         if (result < 0) {
@@ -358,52 +382,50 @@ int videoAudioOpen(JNIEnv *env, jobject surface, const char *path) {
                 break;
             }
             if (tempCC == vc_) {
-//                sws_ = sws_getCachedContext(sws_,
-//                                           frame_->width, frame_->height,
-//                                           (AVPixelFormat) frame_->format,
-//                                           outWidth, outHeight, AV_PIX_FMT_RGBA, SWS_FAST_BILINEAR,
-//                                           0, 0, 0);
-//
-//                if (!sws_) {
-//                    LOGE("sws_getCachedContext FAILD !");
-//                } else {
-//                    uint8_t *data[AV_NUM_DATA_POINTERS] = {0};
-//                    data[0] = (uint8_t *) rgb;
-//                    int lines[AV_NUM_DATA_POINTERS] = {0};
-//                    lines[0] = outWidth * 4;
-//                    int h = sws_scale(sws_, (const uint8_t **) frame_->data, frame_->linesize, 0,
-//                                      frame_->height, data, lines);
+                sws_ = sws_getCachedContext(sws_,
+                                            frame_->width, frame_->height,
+                                            (AVPixelFormat) frame_->format,
+                                            outWidth_, outHeight_, AV_PIX_FMT_RGBA,
+                                            SWS_FAST_BILINEAR,
+                                            0, 0, 0);
+
+                if (!sws_) {
+                    LOGE("sws_getCachedContext FAILD !");
+                } else {
+                    uint8_t *data[AV_NUM_DATA_POINTERS] = {0};
+                    data[0] = (uint8_t *) rgb_;
+                    int lines[AV_NUM_DATA_POINTERS] = {0};
+                    lines[0] = outWidth_ * 4;
+                    int h = sws_scale(sws_, (const uint8_t **) frame_->data, frame_->linesize, 0,
+                                      frame_->height, data, lines);
 //                    LOGE("SLICE HEIGHT %d ", h);
-//                    ANativeWindow_lock(aWindow, &wbuf, 0);
-//                    uint8_t *dst = (uint8_t *) wbuf.bits;
-//                    memcpy(dst, rgb, outWidth * outHeight * 4);
-//                    ANativeWindow_unlockAndPost(aWindow);
-//                    Sleep(40);
+                    ANativeWindow_lock(aWindow, &wbuf_, 0);
+                    uint8_t *dst = (uint8_t *) wbuf_.bits;
+                    memcpy(dst, rgb_, outWidth_ * outHeight_ * 4);
+                    ANativeWindow_unlockAndPost(aWindow);
                 }
-        if (tempCC == ac_) {
-//                if (ac_->channels == 2) {
-//                    LOGE("LINESIZE[0] %d , linesize[1] %d " ,frame_->linesize[0] , frame_->linesize[1] );
-                    uint8_t *out[1] = {0};
-                    out[0] = (uint8_t *) pcm_;
-                    //音频重采样
-                    int len = swr_convert(actx_, out,
-                                          frame_->nb_samples,
-                                          (const uint8_t **) frame_->data,
-                                          frame_->nb_samples);
+            } else if (tempCC == ac_) {
+                uint8_t *out[1] = {0};
+                out[0] = (uint8_t *) pcm_;
+                //音频重采样
+                int len = swr_convert(actx_, out,
+                                      frame_->nb_samples,
+                                      (const uint8_t **) frame_->data,
+                                      frame_->nb_samples);
 //                    LOGE("frame_->pkt_size %d frame_->nb_samples %d ", frame_->linesize[0] , frame_->nb_samples);
-                    //音频部分需要自己维护一个缓冲区，通过他自己回调的方式处理
-            char *pcm_temp = new char[48000 * 4 * 2];
-            memcpy(pcm_temp , pcm_ ,2048 );
-                    audioQue.push(pcm_temp);
-            LOGE(" ADD SIZE %d , " , audioQue.size());
-                    //单声道
-//                    fwrite(out[0], 1, 2048, faout_l);
-//                    fwrite(frame_->data[1] , 1 ,frame_->linesize[1] , faout_r );
-//                }
+                //音频部分需要自己维护一个缓冲区，通过他自己回调的方式处理
+                char *pcm_temp = new char[48000 * 4 * 2];
+                memcpy(pcm_temp, pcm_, 2048);
+                audioQue.push(pcm_temp);
+                audioCount++;
+                //要先缓冲几帧才能播放音频数据
+                if (audioCount == 5) {
+                    playFlag = true;
+                    playOrPauseAudio();
+                }
             }
         }
+
     }
-
-
     return RESULT_SUCCESS;
 }
