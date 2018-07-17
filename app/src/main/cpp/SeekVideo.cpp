dfe375ce3ed37fa4e57b04d82fb4db99fd44b015
admin <273045923@qq.com>
2018-07-17 21:21:34 +0800

    兼容其他的视频，然后seekframe

---
+++ app/src/main/cpp/SeekVideo.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2018/7/17/017.
+//
+
+
+#include <SeekFile.h>
+
+SeekFile::SeekFile() {
+
+}
+
+SeekFile::~SeekFile() {
+
+}
---
+++ app/src/main/cpp/include/SeekFile.h
@@ -0,0 +1,19 @@
+//
+// Created by Administrator on 2018/7/17/017.
+//
+
+#ifndef MYFFMPEG_SEEKFILE_H
+#define MYFFMPEG_SEEKFILE_H
+
+class SeekFile{
+
+private:
+
+public :
+    SeekFile();
+    ~SeekFile();
+
+};
+
+
+#endif //MYFFMPEG_SEEKFILE_H
